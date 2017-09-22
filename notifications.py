540c047198eb9cfd2c7d91594a870128f86843bc
Tiago Taquelim <sikozonbatata@gmail.com>
2017-09-23 00:27:58 +0100

    Add files via upload

--- notifications.py
+++ notifications.py
@@ -1,37 +1,15 @@
 #Python 2
 try:
     import Tkinter as tk
+    from plyer import notification as n
 #Python 3
 except ImportError:
     import tkinter as tk
+    from plyer import notification as n
 
-class NotificationWindow(tk.Frame):
-    """ Notifcations window"""
-
-    def __init__(self, _anime, master = None):
-        super().__init__(master)
-        
-        self.grid(row=0,column=0)
-
-        # Set the notifications window on lower right
-        """
-        w  = 200  
-        screen_width = self.master.winfo_screenwidth()
-        screen_height = self.master.winfo_screenheight()
-        x = (screen_width/2) - (w/2)
-        y = (screen_height/2) - (w/2)
-        self.master.geometry(("%dx%d+%d+%d"% (w, w, x, y)))
-        """
-        self.master.resizable(False,False)
-
-        anime = _anime
-
-        self.create_window(anime)
-
-    def create_window(self, _anime):
-        
-        window = tk.Toplevel(self.master)
-        window.title("Notification %s"%(_anime))
-
-        notification = tk.Label(window, text="%s"%(_anime), padx = 20, pady= 20)
-        notification.grid(row=0,column=0)
+def notify(_anime):
+    notification_logo = "notification.ico"
+    n.notify(title= _anime,
+             message= 'New episode out!',
+             app_name = "Anime Scrapper",
+             app_icon= notification_logo)
